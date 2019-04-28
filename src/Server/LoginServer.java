f31a4f488deedcd22d1472df4a087185ed65736c
Mr00Wang <1617289652@qq.com>
2019-04-28 13:22:31 +0800

    ICWY

---
+++ src/Server/LoginServer.java
@@ -0,0 +1,39 @@
+package Server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class LoginServer implements Runnable{
+
+    private Socket socket = null;
+
+    public LoginServer(Socket socket) {
+        this.socket = socket;
+    }
+
+    // 线程方法
+    public void run() {
+
+
+
+
+    }
+
+    public static void openServer() throws Exception {
+        // 线程池
+        ExecutorService execute = Executors.newFixedThreadPool(2000);
+        // 开启了 TCP 4001 端口 用于登录业务
+        ServerSocket server = new ServerSocket(4001);
+        // 死循环的目的是可以无限服务
+        while (true) {
+            Socket socket = server.accept();
+            socket.setSoTimeout(10000);
+            execute.execute(new LoginServer(socket));
+        }
+
+    }
+
+}
---
+++ src/Server/UserOnlineList.java
@@ -0,0 +1,97 @@
+package Server;
+
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Set;
+
+public class UserOnlineList {
+    private UserOnlineList() {
+    }
+
+    private static UserOnlineList userOnlineList = new UserOnlineList();
+
+    public static UserOnlineList getUserOnlineList() {
+        return userOnlineList;
+    }
+
+    // 我们把所有的在线账户 全部登记在集合中
+    /**
+     * String 是用户的编号
+     */
+    private HashMap<String, UserInfo> hashMap = new HashMap<String, UserInfo>();
+
+    // 注册在线用户
+    public void regOnline(String uid, Socket socket, String email, String phoneNumber) {
+
+        // 判断其他的客户端是否登录一样用户名 如果一样 就强行弄下去
+        UserInfo userInfo = hashMap.get(uid);
+        if (userInfo != null) {
+            try {
+                try {
+                    userInfo.getSocket().getOutputStream().write(4);
+                } catch (Exception e) {
+                }
+                userInfo.getSocket().close();
+            } catch (Exception e) {
+            }
+        }
+
+        userInfo = new UserInfo();
+        userInfo.setUid(uid);
+        userInfo.setEmail(email);
+        userInfo.setPhone(phoneNumber);
+        userInfo.setSocket(socket);
+        hashMap.put(uid, userInfo);// 登记在线
+    }
+
+    /**
+     * 更新客户端的UDP信息
+     *
+     * @param uid
+     *            用户编号
+     * @param ip
+     *            udp IP地址
+     * @param port
+     *            udp端口
+     * @throws NullPointerException
+     *             空指针异常
+     */
+    public void updateOnlineUDP(String uid, String ip, int port) throws NullPointerException {
+        UserInfo userInfo = hashMap.get(uid);
+        userInfo.setUdpip(ip);
+        userInfo.setUdpport(port);
+    }
+
+    // 判断用户是否在线 如果是true 在线 false 不在线
+    public boolean isUserOnline(String uid) {
+        return hashMap.containsKey(uid);
+    }
+
+    /**
+     * 获得在线用户信息
+     *
+     * @param uid
+     * @return
+     */
+    public UserInfo getOnlineUserInfo(String uid) {
+        return hashMap.get(uid);
+    }
+
+    /**
+     * 下线了
+     *
+     * @param uid
+     */
+    public void logout(String uid) {
+        hashMap.remove(uid);
+    }
+
+    /**
+     * 获得所有的在线信息
+     *
+     * @return
+     */
+    public Set<String> getUserInfos() {
+        return hashMap.keySet();
+    }
+}
